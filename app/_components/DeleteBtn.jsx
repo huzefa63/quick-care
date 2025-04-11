6df5d0b927ca98091c5bcdfde015f7cbdc1efabc
learner <huzefaratlam63@gmail.com>
2025-04-11 19:53:21 +0530

    Update DeleteBtn.jsx

--- app/_components/DeleteBtn.jsx
+++ app/_components/DeleteBtn.jsx
@@ -1,12 +1,18 @@
 'use client';
-import { MdDelete } from "react-icons/md";
 
 import { deleteAccountAction } from "@/actions/deleteAccountAction";
+import { useRef } from "react";
 
 function DeleteBtn() {
+  const ref = useRef(null);
+  function handleSubmit(e){
+    e.preventDefault();
+    const confirmed = window.confirm('are you sure you want to delete your account ?');
+    if(confirmed) ref.current.requestSubmit();
+  }
     return (
-      <form action={deleteAccountAction}>
-        <button className="bg-red-500 gap-2 text-white px-4 py-2 flex items-center">
+      <form ref={ref} action={deleteAccountAction}>
+        <button onClick={handleSubmit} className="bg-red-500 gap-2 text-white px-4 py-2 flex items-center">
           <MdDelete />
           Delete Account
         </button>
