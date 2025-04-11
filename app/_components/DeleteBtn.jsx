6de49e37a91e53c79f29f266dff0fd99cd33b978
learner <huzefaratlam63@gmail.com>
2025-04-11 19:39:21 +0530

    Create DeleteBtn.jsx

---
+++ app/_components/DeleteBtn.jsx
@@ -0,0 +1,16 @@
+'use client';
+
+import { deleteAccountAction } from "@/actions/deleteAccountAction";
+
+function DeleteBtn() {
+    return (
+      <form action={deleteAccountAction}>
+        <button className="bg-red-500 gap-2 text-white px-4 py-2 flex items-center">
+          <MdDelete />
+          Delete Account
+        </button>
+      </form>
+    );
+}
+
+export default DeleteBtn
