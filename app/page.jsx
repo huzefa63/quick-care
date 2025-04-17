8baaacb44e87ee4a0b89e3616dadbd7d0c6e72da
learner <huzefaratlam63@gmail.com>
2025-04-17 15:12:43 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -7,8 +7,9 @@ export const metadata = {
 }
 
 export default async function Home() {
-  // const session = await auth();
-  // if(session) redirect('/app/appointments');
+  const session = await auth();
+  console.log(session);
+  if(session) redirect('/app/appointments');
   return (
     <div className=" h-full overflow-hidden border">
       <HomepageMain />
