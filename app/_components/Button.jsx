b211af1e931c85dc1a85a70bfed409cd4552da02
learner <huzefaratlam63@gmail.com>
2025-04-17 18:11:52 +0530

    Update Button.jsx

--- app/_components/Button.jsx
+++ app/_components/Button.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 function Button({children,session}) {
     console.log(session);
-    if(session){
+    if(!session){
         return (
           <form action={signInAction}>
             <button className="bg-cyan-400 text-white px-5 shadow-sm text-lg py-3 rounded-sm tracking-widest ">
